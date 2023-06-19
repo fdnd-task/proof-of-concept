5fa5817aefb6619142c1d67eb904bd8832747f78
Stefan Espant <info@espant.com>
2023-06-19 14:00:25 +0200

    v1.4.7
    logo toegankelijk gemaakt voor safari

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,12 @@
 const interfaceSound = new Audio("./assets/interface-124464.mp3");
-const interfaceClick = document.querySelector(".interface-link");
+const interfaceClick = document.querySelector(".default-link");
 
-interfaceClick.addEventListener("click", () => {
+interfaceClick.addEventListener("click", (event) => {
+    event.preventDefault(); // Voorkomt dat de standaard linkactie wordt uitgevoerd
+
     interfaceSound.play();
+
+    setTimeout(() => {
+        window.location.href = event.target.href; // Doorsturen naar de volgende pagina
+    }, 800); // Wacht 0.8 seconden voordat de doorstuuring plaatsvindt
 });
