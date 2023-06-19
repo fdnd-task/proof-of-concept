9fc58b677cca5ffd058c80bfcb1477d6c8f37870
Stefan Espant <info@espant.com>
2023-06-19 19:09:27 +0200

    v2.0
    intro lottiefiles animatie toegevoegd

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,12 +1,14 @@
 const interfaceSound = new Audio("./assets/interface-124464.mp3");
-const interfaceClick = document.querySelector(".default-link");
+const interfaceClicks = document.querySelectorAll(".default-link");
 
-interfaceClick.addEventListener("click", (event) => {
+interfaceClicks.forEach((interfaceClick) => {
+  interfaceClick.addEventListener("click", (event) => {
     event.preventDefault(); // Voorkomt dat de standaard linkactie wordt uitgevoerd
 
     interfaceSound.play();
 
     setTimeout(() => {
-        window.location.href = event.target.href; // Doorsturen naar de volgende pagina
+      window.location.href = event.target.href; // Doorsturen naar de volgende pagina
     }, 800); // Wacht 0.8 seconden voordat de doorstuuring plaatsvindt
+  });
 });
