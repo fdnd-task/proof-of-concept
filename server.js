7dec689c4c3126bc1b6c9a7e78f3b428f66fcd92
Sidopjescherm <hermussidney@gmail.com>
2025-06-04 22:49:33 +0200

    #25 Opzet server
    heir is het begin van mijn server file, maar het wil nog niet likken

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from 'express'
+
+import { Liquid } from 'liquidjs';
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express()
+
+  const response = await fetch ('https://the-sprint-api.onrender.com/people')
+
+  app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://the-sprint-api.onrender.com",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/api": "",
+    },
+    onProxyReq: (proxyReq, req, res) => {
+      proxyReq.removeHeader("x-api-key");
+      proxyReq.setHeader("x-api-key", process.env.API_KEY);
+    },
+  })
+);
+  console.log(response)
+
+app.set('port', process.env.PORT || 7000)
+
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
