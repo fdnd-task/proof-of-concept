919705c2e77a1581297e10b0384b88481ffa2f53
Roxy <roxyfokker@hotmail.com>
2026-06-03 16:13:56 +0200

    feat: exhibitroute opzetten met data ophalen en redirect van / #7

--- server.js
+++ server.js
@@ -11,10 +11,30 @@ const engine = new Liquid()
 app.engine('liquid', engine.express())
 app.set('views', './views')
 
-const baseUrl = ''
+const exhibitUrl       = 'https://fdnd-agency.directus.app/items/teylers_museum_exhibits'
+const sectionsUrl      = 'https://fdnd-agency.directus.app/items/teylers_museum_exhibits_sections'
+const personsUrl       = 'https://fdnd-agency.directus.app/items/teylers_museum_persons'
+const sourcesUrl       = 'https://fdnd-agency.directus.app/items/teylers_museum_sources'
+const quizQuestionsUrl = 'https://fdnd-agency.directus.app/items/teylers_museum_quiz_questions'
+const quizAnswersUrl   = 'https://fdnd-agency.directus.app/items/teylers_museum_quiz_answers'
+const quizAttemptsUrl  = 'https://fdnd-agency.directus.app/items/teylers_museum_quiz_attempts'
 
 app.get('/', async function (request, response) {
-  response.render('home.liquid')
+  const fetchResponse = await fetch(exhibitUrl)
+  const fetchResponseJSON = await fetchResponse.json()
+
+  response.redirect(`/exhibits/${fetchResponseJSON.data[0].slug}`)
+})
+
+app.get('/exhibits/:slug', async function (request, response) {
+
+  const fetchResponse = await fetch(`${exhibitUrl}?filter[slug][_eq]=${request.params.slug}`)
+  const fetchResponseJSON = await fetchResponse.json()
+  const exhibit = fetchResponseJSON.data[0]
+
+  response.render('home.liquid', { 
+    exhibit 
+  })
 })
 
 app.get('/niet-beschikbaar', async function (request, response) {
